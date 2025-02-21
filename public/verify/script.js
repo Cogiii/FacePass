df4f86ab242dd06da735bf0f2a9c8b272a545f7f
Cogiii <laurencedevera8@gmail.com>
2025-02-21 19:25:10 +0800

    added landmark68TinyNet Model

--- public/verify/script.js
+++ public/verify/script.js
@@ -30,7 +30,8 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models')
 ]).then(startCamera);
 
 /*
